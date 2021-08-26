3eb2e9306a00f42b3410da9df99806978d6736c6
kedarvyas <kedar.m.vyas@gmail.com>
2021-08-26 13:02:23 -0500

    uploading files

---
+++ src/components/About.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1"
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    const one = (
+      <p>
+        I am a senior studying <b>Computer Engineering</b> at the {" "}
+        <b> University of Tennessee</b>.
+      </p>
+    );
+    const two = (
+      <p>
+        I'm into <b>frontend development</b>, <b>ui/ux design</b> and{" "}
+        <b>web design</b>. In my free time, my interests include sports, tech, music, and playing video games.
+      </p>
+    );
+    const three = (
+      <p>
+        <b>Want to chat?</b> Shoot me a message at{" "}
+        <a href="mailto:kvyas1@vols.utk.edu">
+          kvyas1@vols.utk.edu
+        </a>{" "}
+        and let's talk.
+      </p>
+    );
+    const desc_items = [one, two];
+
+    const tech_stack = [
+      "HTML & CSS",
+      "C++",
+      "JavaScript",
+      "Python",
+      "ReactJS",
+      "Node.js",
+      "Git",
+      "Windows, MacOS, Unix"
+    ];
+
+    const tech_items = tech_stack.map(stack => <li>{stack}</li>);
+    var image = require("./assets/me.jpg");
+
+    return (
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {desc_items}
+              {"programming skills and operating systems:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="about-image">
+              <img src={image} />
+            </div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Intro.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import Typical from "react-typical";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <Typist avgTypingDelay={120}>
+          <span className="intro-title">
+            {"hey there, i am "}
+            <span className="intro-name">{"kedar"}</span>
+            {"."}
+          </span>
+        </Typist>
+        <FadeInSection>
+          <div className="intro-subtitle">
+            i am a{' '}
+            <Typical className="intro-type"
+              loop={3}
+              wrapper="b"
+              steps={[
+                'developer.',
+                1500,
+                'problem solver.',
+                750,
+                'fast learner.',
+                750,
+                'tech enthusiast.',
+                750,
+                'sports fanatic.',
+                750,
+                'computer scientist.',
+                5000
+              ]}
+            />
+          </div>
+          <div className="intro-desc">i code sometimes</div>
+          <a
+            href="mailto:kvyas1@vols.utk.edu"
+            className="intro-contact"
+          >
+           <EmailRoundedIcon></EmailRoundedIcon> 
+            
+            {"  " + "let's get in touch."}
+          </a>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default Intro;
